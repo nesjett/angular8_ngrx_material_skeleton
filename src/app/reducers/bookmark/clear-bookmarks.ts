fbd389775ff9fcc4064fdaa6cb1d9b3bf4490596
Néstor Sabater <web.nsabater@gmail.com>
2020-07-02 02:40:59 +0200

    initial commit

---
+++ src/app/reducers/bookmark/clear-bookmarks.ts
@@ -0,0 +1,3 @@
+export const clearState = (state) => {
+    return [];
+}
---
+++ src/app/types/group.ts
@@ -0,0 +1,6 @@
+import { Bookmark } from './bookmark';
+
+export interface Group {
+    name: string,
+    bookmarks: Array<Bookmark>
+}
